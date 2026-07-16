0fc5169979a0b281bfec5bdb0537f567eecc6122
Mohamed Nezhari <109751834+Nezgova@users.noreply.github.com>
2026-07-16 16:28:03 +0200

    springboot created

---
+++ backend/nezai/src/main/java/org/example/nezai/NezaiApplication.java
@@ -0,0 +1,13 @@
+package org.example.nezai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NezaiApplication.class, args);
+    }
+
+}
---
+++ backend/nezai/src/test/java/org/example/nezai/NezaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nezai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
