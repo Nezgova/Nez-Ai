34db10cc184a2ab76ab934cc437309b64a6adbed
Mohamed Nezhari <109751834+Nezgova@users.noreply.github.com>
2026-07-19 14:35:02 +0200

    .

--- backend/nezai/src/main/java/org/example/nezai/controller/TestController.java
+++ backend/nezai/src/main/java/org/example/nezai/controller/TestController.java
@@ -1,4 +1,4 @@
-package org.example.nezai;
+package org.example.nezai.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
